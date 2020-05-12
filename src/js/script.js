502c9f81f631e1368ab21fc25a08cee7fe3c1fa4
Masha <example@gmail.com>
2020-05-12 15:51:23 +0300

    carousel with slick

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
-    $('.carousel__inner').slick();
-  });
+    $('.carousel__inner').slick(
+      {
+        speed: 300,
+        /* adaptiveHeight: true, */
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/arrow_right.png"></button>',
+        responsive: [ 
+          {
+              breakpoint: 768,
+              settings: {
+              dots: true
+              arrows: false
+            }  
+                    
+        ]
+
+      });
+  }); 
