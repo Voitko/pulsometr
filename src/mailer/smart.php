8e955280770f6aa24285a049cae437afe7f3bf9c
Masha <example@gmail.com>
2020-05-30 16:46:14 +0300

    the end

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -14,12 +14,12 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'voitko177@gmail.com';                 // Наш логин
-$mail->Password = 'voitko2001';                           // Наш пароль от ящика
+$mail->Password = 'Voitko2001';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
 $mail->setFrom('voitko177@gmail.com', 'Pulse');   // От кого письмо 
-$mail->addAddress('yabisog265@aprimail.com');     // Add a recipient
+$mail->addAddress('voytko122@provlst.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
